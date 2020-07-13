e2e82b449ccd89be49cc67f1031f268ad9347663
Paeng <gwpaeng@naver.com>
2020-07-13 03:14:43 +0000

    to get the jobs-id in stackoverflow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from indeed import get_jobs as get_indeed_jobs 
+from so import get_jobs as get_so_jobs
 
-indeed_jobs = get_indeed_jobs() 
-print(indeed_jobs)
+
+# indeed_jobs = get_indeed_jobs() 
+so_jobs = get_so_jobs()
--- so.py
+++ so.py
@@ -3,3 +3,26 @@ from bs4 import BeautifulSoup #html에서 정보를 추출하기 위한 package
 
 URL = f"https://stackoverflow.com/jobs?q=python&sort=i"
 
+def get_last_page():
+  result = requests.get(URL)
+  soup = BeautifulSoup(result.text, "html.parser")
+  pages = soup.find("div", {"class" : "s-pagination"}).find_all("a")
+  last_page = pages[-2].get_text(strip=True)
+  return int(last_page)
+
+
+def extract_jobs(last_page):
+  jobs = [] 
+  for page in range(last_page):
+    result = requests.get(f"{URL}&pg={page+1}")
+    # print(result.status_code) # 확인 코드 잘 되면 200 출력
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class":"-job"})
+    for result in results:
+      print(result["data-jobid"])
+
+
+def get_jobs():
+  last_page = get_last_page()
+  jobs = extract_jobs(last_page)
+  return 
