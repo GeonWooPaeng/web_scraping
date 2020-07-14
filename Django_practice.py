7c1d0a3b25f04cf7d8d533ba76dc38f6dfc2148e
Paeng <gwpaeng@naver.com>
2020-07-14 10:26:57 +0000

    Django_study

---
+++ Django_practice.py
@@ -0,0 +1,36 @@
+# 함수에서 무한 argument 사용 
+#1. *args(positional argument - 숫자)
+# tuple 
+# def plus(a,b,*args): 
+#   pass 
+
+#ex
+#무한 계산기 만들기
+# def plus(*args):
+#   result = 0 
+#   for number in args:
+#     result += number 
+#   print(result)
+  
+
+#2. **kwargs(keyword argument - 문자)
+# dictonary
+# def plus(a,b,**kwargs): 
+#   pass 
+
+
+
+
+#객체 지향 
+# class Car():
+#   #blue print
+#   wheels = 4 
+#   doors = 4 
+#   windows = 4 
+#   seats = 4 
+
+# porche = Car() # instance(porche), instantiation(Car뒤의 ()붙은 것)
+
+# class안에 있는 function이 method이다
+
+
